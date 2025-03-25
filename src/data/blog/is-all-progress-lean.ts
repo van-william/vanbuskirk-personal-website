ef4b43a1bc10e7eefb72505edec2ffb239c813e1
William VanBuskirk <william.n.vanbuskirk@gmail.com>
2025-03-25 16:08:43 -0500

    published new blog

---
+++ src/data/blog/is-all-progress-lean.ts
@@ -0,0 +1,62 @@
+
+import { BlogPost } from "@/utils/markdown";
+
+export const post: BlogPost = {
+  id: "3",
+  title: "Is all growth lean?",
+  slug: "is-all-growth-lean",
+  date: "2025-03-24",
+  excerpt: "Is building software just like building muscle?",
+  coverImage: "https://images.unsplash.com/photo-1458014854819-1a40aa70211c",
+  tags: ["product"],
+  content: `
+# Is All Growth Lean?
+## Are There Parallels Between Muscle Building and Product Development?
+
+In fitness, there's a well-known strategy called "bulking and cutting." This approach involves intentionally gaining weight (both muscle and fat) before strategically reducing body fat to develop lean muscle. Recently, I found myself drawing a parallel between this concept and the process of product development.
+
+The bulking and cutting cycle in fitness involves increasing complexity (adding mass) before refining and simplifying (cutting fat). Why take this approach? Because it is notoriously difficult to build only lean muscle. Similarly, in product development, it is equally challenging to build only value-add features without some degree of excess.
+
+This intentional cycle of increasing complexity followed by strategic simplification can be observed in other areas as well.
+
+## Feature Bulking
+In the early stages of product development, teams often engage in a "feature bulking" phase. This involves:
+
+1. **Exploration and Ideation**: Developers and product managers brainstorm and implement various features, some of which may not make it to the final product.
+2. **Prototype Development**: Creating multiple prototypes or minimum viable products (MVPs) to test different concepts and functionalities.
+3. **User Feedback Collection**: Gathering extensive user feedback on various features and use cases.
+4. **Technology Experimentation**: Trying out different technologies, frameworks, or architectural approaches.
+
+*This phase is characterized by an increase in product complexity as new features and functionalities are added—much like how muscle mass and overall body weight increase during a bulking phase in bodybuilding.*
+
+## Feature Cutting
+
+Once the initial exploration and development phase is complete, product teams enter a "feature cutting" phase:
+
+1. **Feature Prioritization**: Identifying core features that provide the most value to users and align with the product vision.
+2. **Simplification**: Streamlining the user interface and experience by removing unnecessary elements.
+3. **Performance Optimization**: Refining the codebase and architecture to improve speed and efficiency.
+4. **Focus on Core Value Proposition**: Ensuring that the product clearly delivers on its primary promise to users.
+
+This phase parallels the cutting phase in bodybuilding when individuals trim fat and become leaner.
+
+### The Big Idea
+Pure lean growth is difficult; adding complexity is sometimes a necessary prerequisite. Here are a few examples where this cycle applies beyond fitness and product development:
+
+- **Scientific Research**: Theories often become more complex as new data is incorporated. Eventually, a simpler, more elegant theory may emerge that explains phenomena more effectively.
+- **Writing and Editing**: Authors often begin with a "bulky" first draft, including numerous ideas, scenes, and characters. The editing process then refines this complexity into a more focused final product.
+- **Organizational Structure**: Companies may go through periods of expansion, adding departments and roles, before realizing the need to streamline for better efficiency.
+- **Manufacturing Processes**: Production lines often start with numerous steps and components, which are later optimized for efficiency—exemplified by Toyota's lean manufacturing principles.
+
+Embracing this cycle of increasing and then decreasing complexity offers several advantages. A "bulk" phase allows teams to explore options and innovations comprehensively. Additionally, gathering customer insights can be challenging when building fewer features slowly. Increasing complexity in the short term can provide a more holistic experience in the long run. Finally, iterative development is key—staying too lean for too long can stifle growth and innovation.
+
+### Applying Bulking and Cutting to Product Development
+- **Embrace Exploration**: The bulk phase of product development is all about innovation. Not all AI features will succeed, but casting a wide net helps define the roadmap for deep investment.
+- **Refine with Context**: Sometimes, it's not just about data—customer interviews provide crucial context for simplifying the customer experience.
+
+This is especially true for features relying on emerging technologies like AI models. Uncertainty is inevitable, and experimentation is essential. Not all features should be treated as permanent—there is a time to cut and simplify. A few years ago, chat interfaces were everywhere; now, AI agents are the focus. Knowing when to trim excess is crucial.
+
+### Final Thought
+The path to simplicity leads through complexity. Embrace the process, and trust that the final "cut" will reveal something truly remarkable.
+`,
+};
---
+++ src/data/draft-blog/metadata-driven-data-engineering.ts
@@ -0,0 +1,15 @@
+
+import { BlogPost } from "@/utils/markdown";
+
+export const post: BlogPost = {
+  id: "4",
+  title: "Metadata Driven Data Engineering",
+  slug: "metadata-driven-data-engineering",
+  date: "2025-03-25",
+  excerpt: "Properly using metadata for data transformations enables repetable workflows with the right context",
+  coverImage: "https://plus.unsplash.com/premium_photo-1684341008404-af4df3d54615",
+  tags: ["python", "spark"],
+  content: `
+
+`,
+};
